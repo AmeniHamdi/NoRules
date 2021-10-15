328a5929c34043a26a1dee06e31f5b80d97f1892
AmeniHamdi <44673905+AmeniHamdi@users.noreply.github.com>
2021-10-16 01:32:09 +0200

    Tcommun

--- src/infotourisme/TransportCommun.java
+++ src/infotourisme/TransportCommun.java
@@ -10,5 +10,67 @@ package infotourisme;
  * @author Ameni Hamdi
  */
 public class TransportCommun {
+    private int idTransportCommun;
+    private Float prixBillet;
+    private int capacité;
+    private String Type ;
+
+    public TransportCommun() {
+    }
+
+    public TransportCommun(int idTransportCommun, Float prixBillet, int capacité, String Type) {
+        this.idTransportCommun = idTransportCommun;
+        this.prixBillet = prixBillet;
+        this.capacité = capacité;
+        this.Type = Type;
+    }
+
+    public TransportCommun(Float prixBillet, int capacité, String Type) {
+        this.prixBillet = prixBillet;
+        this.capacité = capacité;
+        this.Type = Type;
+    }
+
+    public int getIdTransportCommun() {
+        return idTransportCommun;
+    }
+
+    public void setIdTransportCommun(int idTransportCommun) {
+        this.idTransportCommun = idTransportCommun;
+    }
+
+    public Float getPrixBillet() {
+        return prixBillet;
+    }
+
+    public void setPrixBillet(Float prixBillet) {
+        this.prixBillet = prixBillet;
+    }
+
+    public int getCapacité() {
+        return capacité;
+    }
+
+    public void setCapacité(int capacité) {
+        this.capacité = capacité;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String Type) {
+        this.Type = Type;
+    }
+
+    @Override
+    public String toString() {
+        return "TransportCommun{" + "idTransportCommun=" + idTransportCommun + ", prixBillet=" + prixBillet + ", capacit\u00e9=" + capacité + ", Type=" + Type + '}';
+    }
+    
+    
+    
+    
+    
     
 }
